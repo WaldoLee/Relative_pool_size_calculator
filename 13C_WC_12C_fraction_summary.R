4648bb1cf1f481fb26149b29a1eb83ff63e59ef1
WaldoLee <wdglee@snu.ac.kr>
2017-04-25 11:15:19 +0300

    Initial commit

---
+++ 13C_WC_12C_fraction_summary.R
@@ -0,0 +1,78 @@
+library(xlsx)
+#############################################################################
+# Input data
+neg_source <- "C://Users/biology/Desktop/LAB DB/Project_ing/Spatial fluxomics/2. Rapid fractionation_validation/RERUN_20160929_13Gln_13Glc_WC_MT_CY_neg/RERUN_20160929_13Gln_13Glc_WC_MT_CY_neg.csv"
+neg_table <- read.csv(neg_source, header = TRUE)
+head(neg_table)
+neg_table <- neg_table[, c(8, 9, 15, 16, 18, 19, 21, 22, 24, 25, 27, 28, 30, 31, 33, 34, 36, 37, 39, 40, 42, 43)]
+neg_table <- as.matrix(neg_table)
+#############################################################################
+# R_calculator
+R_calculator <- function (col_num = 3) {
+  I_nonlabeled <- numeric()
+  I_labeled <- numeric()
+  R <- numeric()
+  labeled_set <- vector('numeric')
+  result_vector <- vector()
+  result_matrix <- vector()
+  
+  for (i in 1:nrow(neg_table)) {
+    if (neg_table[i, 1] == "C12 PARENT") {
+      if (i == 1) {
+      } else {
+        I_labeled <- sum(labeled_set)
+        R <- I_labeled/I_nonlabeled
+        result_vector <- c(result_vector, I_nonlabeled)
+        result_vector <- c(result_vector, I_labeled)
+        result_vector <- c(result_vector, R)
+        #print(result_vector)
+        result_matrix <- rbind(result_matrix, result_vector)
+        }
+      metabolite <- as.character(neg_table[i, 2])
+      result_vector <- vector()
+      I_nonlabeled <- numeric()
+      I_labeled <- numeric()
+      R <- numeric()
+      result_vector <- c(result_vector, metabolite)
+      I_nonlabeled <- as.numeric(neg_table[i, col_num])
+      labeled_set <- vector('numeric')
+    } else {
+      labeled_set <- c(labeled_set, as.numeric(neg_table[i, col_num]))
+    }
+  }
+  
+  I_labeled <- sum(labeled_set)
+  R <- I_labeled/I_nonlabeled
+  result_vector <- c(result_vector, I_nonlabeled)
+  result_vector <- c(result_vector, I_labeled)
+  result_vector <- c(result_vector, R)
+  result_matrix <- rbind(result_matrix, result_vector)
+  colnames(result_matrix) <- c("compound", "nonlabeled", "labeled", "R")
+  rownames(result_matrix) <- NULL
+  result_matrix[, c(2,3,4)] <- as.numeric(result_matrix[, c(2,3,4)])
+  result_df <- as.data.frame(result_matrix)
+  return(result_df)
+}
+#############################################################################
+create_result_summary_df <- function(){
+  result_summary <- vector()
+  for (i in 3:ncol(neg_table)) {
+    result_table <- R_calculator(i)
+    result_table_trim <- result_table[,4]
+    result_table_trim <- as.vector(result_table_trim)
+    result_summary <- rbind(result_summary, result_table_trim)
+  }
+  colnames(result_summary) <- R_calculator(3)[,1]
+  #rownames(result_summary) <- substr(colnames(neg_table)[3:ncol(neg_table)], 27, 30)
+  rownames(result_summary) <- colnames(neg_table)[3:ncol(neg_table)]
+  result_summary_df <- as.data.frame(result_summary)
+  result_summary_df <- data.frame(t(result_summary_df))
+  return(result_summary_df)
+}
+
+final_result <- create_result_summary_df()
+#############################################################################
+write.xlsx(x = final_result, file = "C://Users/biology/Desktop/LAB DB/Project_ing/Spatial fluxomics/2. Rapid fractionation_validation/RERUN_20160929_13Gln_13Glc_WC_MT_CY_neg/20160929_13C_12C_summary.xlsx", sheetName = "TestSheet", row.names = TRUE)
+#############################################################################
+
+?substr
